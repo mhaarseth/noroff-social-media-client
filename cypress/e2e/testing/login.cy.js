d35e0c0a5e00f1f6d9e341479d8e9ffeacd54de8
mhaarseth <100208301+mhaarseth@users.noreply.github.com>
2023-11-13 23:31:47 +0100

    Update login.cy.js

--- cypress/e2e/testing/login.cy.js
+++ cypress/e2e/testing/login.cy.js
@@ -2,7 +2,7 @@ describe("login", () => {
   it("should disallow login with invalid credentials, and show the user a message", () => {
     cy.wait(500);
     cy.visit("/");
-    cy.get(".modal-footer>button").eq(1).click();
+    cy.get("#registerForm>.modal-footer>button").eq(1).click();
     cy.get("#loginForm").should("be.visible");
     cy.get("#loginEmail").type("noroff@stud.noroff.no");
     cy.wait(500);
